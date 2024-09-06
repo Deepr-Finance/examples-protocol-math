59aa9db0093129d752f2aad2337a293775b48706
Javier Belbruno <javierbelbruno@gmail.com>
2024-09-06 10:09:12 +0000

    Add viemConfig.

--- constants.js
+++ constants.js
@@ -1,6 +1,32 @@
 const networkConfig = {
     iotaevm : {
         rpc: "https://json-rpc.evm.iotaledger.net",
+        viemConfig: {
+            id: 8822,
+            name: 'iotaevm',
+            network: 'iotaevm',
+            nativeCurrency: {
+                decimals: 18,
+                name: 'IOTA',
+                symbol: 'IOTA',
+            },
+            rpcUrls: {
+                default: {
+                    http: ['https://json-rpc.evm.iotaledger.net'],
+                    webSocket: ['wss://ws.json-rpc.evm.iotaledger.net'],
+                },
+                public: {
+                    http: ['https://json-rpc.evm.iotaledger.net'],
+                    webSocket: ['wss://ws.json-rpc.evm.iotaledger.net'],
+                },
+            },
+            blockExplorers: {
+                default: {
+                name: 'Explorer',
+                url: 'https://explorer.evm.iota.org/',
+                },
+            },
+        },
         addresses : {
             dWIOTA: '0x260817581206317E2665080A2E66854e922269d0',
             wIOTA: '0x6e47f8d48a01b44DF3fFF35d258A10A3AEdC114c'
